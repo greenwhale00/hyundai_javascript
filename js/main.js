423615946002dfe7411c8e60103960610ae681ed
greenwhale00 <goreakay@gmail.com>
2022-09-01 14:00:28 +0900

    220901

---
+++ js/main.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', () => {
+    ////////////////////////////////////////////////////
+
+
+
+    //document.querySelector('.Wrap').style.color = 'red';
+    //$('.Wrap').css("color", "red")
+
+    document.querySelector('.top_close_btn').addEventListener('click', function () {
+        document.querySelector('.TopBanner').classList.add('on')
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ////////////////////////////////////////////////////
+});
+
